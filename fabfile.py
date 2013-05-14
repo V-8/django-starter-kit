9aca0c00bf92e209a3ef7ffaf4c7b8c9270b810d
creativito <vivito@126.com>
2013-05-15 00:54:49 +0800

    fabfile.py modified.

--- fabfile.py
+++ fabfile.py
@@ -68,7 +68,7 @@ def setup():
         local('git remote add origin %s@%s:%s' % ('user_name', 'host_ip_address', repo_project_dir))
     with cd(deploy_dir):
         run('rm -rf env_project_name && mkdir env_project_name && cd env_project_name')
-        run('git clone %s' % (repo_project_dir))
+        run('git clone %s' % repo_project_dir)
     setup_deploy_env()
 
 
